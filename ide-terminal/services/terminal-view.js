f89241fe68f0d355e63d80d27a5d5b68f0e35340
delchev <delchevn@gmail.com>
2017-09-26 00:02:04 +0300

    view description enhanced

--- ide-terminal/services/terminal-view.js
+++ ide-terminal/services/terminal-view.js
@@ -1,6 +1,10 @@
 exports.getView = function() {
 	var view = {
+			"id":"terminal",
 			"name":"Terminal",
+			"factory":"frame",
+			"region":"center-bottom",
+			"label":"Terminal",
 			"link":"../ide-terminal/terminal.html"
 	};
 	return view;
